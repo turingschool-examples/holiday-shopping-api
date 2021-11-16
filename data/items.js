a8b0aa81a1c9f7f6a12c1e913d004ca341434fdf
Hannah Hudson <hannah.hch@gmail.com>
2021-11-16 12:02:24 -0700

    First commit

---
+++ data/items.js
@@ -0,0 +1,88 @@
+let items = [
+  {
+    id: 1,
+    recipient: 'Scott',
+    name: 'Gift card to his favorite tattoo shop',
+    link: '',
+    priceInDollars: 100
+  },
+  {
+    id: 2,
+    recipient: 'Kayla',
+    name: 'Fancy Tiger Crafts gift card',
+    link: 'https://fancytigercrafts.com/products/gift-card?variant=32257472069710',
+    priceInDollars: 30
+  },
+  {
+    id: 3,
+    recipient: 'Hannah',
+    name: '"Fifty Place to Dive Before You Die"',
+    link: 'https://www.amazon.com/Fifty-Places-Dive-Before-You/dp/158479710X/ref=pd_bxgy_1/132-0874346-7029366?pd_rd_w=nDRmB&pf_rd_p=c64372fa-c41c-422e-990d-9e034f73989b&pf_rd_r=9VAG7HDBZ4P7QV8VG796&pd_rd_r=6b47c525-bf8e-43ee-847a-b47b49b55329&pd_rd_wg=fT5Fu&pd_rd_i=158479710X&psc=1',
+    priceInDollars: 12.35
+  },
+  {
+    id: 4,
+    recipient: 'Leta',
+    name: '"The World\'s Most Haunted Places"',
+    link: 'https://www.amazon.com/Worlds-Most-Haunted-Places-Revised/dp/1601631936',
+    priceInDollars: 17.95
+  },
+  {
+    id: 5,
+    recipient: 'Nik',
+    name: 'Wine tasting',
+    link: '',
+    priceInDollars: 30
+  },
+  {
+    id: 6,
+    recipient: 'Jeremiah',
+    name: 'Sketchbook',
+    link: 'https://www.barnesandnoble.com/w/moleskine-art-watercolor-block-large-black-moleskine/1131270962;jsessionid=FDD36DF1102D58F1AE812DB066769C1D.prodny_store02-atgap15?ean=8053853603234',
+    priceInDollars: 22.95
+  },
+  {
+    id: 7,
+    recipient: 'Heather',
+    name: 'Sky diving lessons',
+    link: '',
+    priceInDollars: 200
+  },
+  {
+    id: 8,
+    recipient: 'Cass',
+    name: '"Bob\'s Burgers" DVD',
+    link: '',
+    priceInDollars: 25
+  },
+  {
+    id: 9,
+    recipient: 'Robbie',
+    name: 'Heavy-duty stand mixer',
+    link: 'https://www.wayfair.com/kitchen-tabletop/pdp/kitchenaid-artisan-series-10-speed-5-qt-stand-mixer-w005013135.html?domain=thespruceeats.com&cjevent=25338cbd466811ec8348001e0a1c0e14&refID=CJ314743-CJ5370367&PID=CJ100101292&clickid=25338cbd466811ec8348001e0a1c0e14&cjdata=MXxOfDB8WXww',
+    priceInDollars: 430
+  },
+  {
+    id: 10,
+    recipient: 'Travis',
+    name: 'Boxing gloves',
+    link: 'https://www.walmart.com/ip/Venum-Contender-2-0-Boxing-Gloves/470207607',
+    priceInDollars: 39.97
+  },
+  {
+    id: 11,
+    recipient: 'Will',
+    name: 'Wingsuit rental',
+    link: '',
+    priceInDollars: 150
+  },
+  {
+    id: 12,
+    recipient: 'Taylor',
+    name: 'A local book store gift card',
+    link: '',
+    priceInDollars: 30
+  }
+];
+module.exports = items
+
